f271d65c2c53685121a4ad248c1313f6c2449d2c
susmita218 <susmita218@gmail.com>
2021-09-06 02:31:54 +0530

    update in code commit

--- src/Date.java
+++ src/Date.java
@@ -12,11 +12,20 @@ public class Date {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Susmita\\chromedriver_win32_update\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.path2usa.com/travel-companions");
-		//April14
+		//December23
 		driver.findElement(By.xpath("//input[@name='travel_date']")).click();
+		
+		//first check the month
+		WebElement month=driver.findElement(By.cssSelector("div[class='datepicker-days'] th[class='datepicker-switch']"));
+		while (!month.getText().contains("December"))
+		{
+		driver.findElement(By.cssSelector(".datepicker-days th[class='next']")).click();
+		}
+		
+		//check the day of the month
 		List<WebElement> dates=driver.findElements(By.className("day"));
 		int count=dates.size();
-		
+		System.out.println(count);
 		for(int i=0; i<count; i++)
 		{
 			String text=dates.get(i).getText();
